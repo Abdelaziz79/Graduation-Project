5d477c2515688c6e51c0beb06155304f77fade78
unknown <abdelazizelhadry1@gmail.com>
2024-02-22 15:06:57 +0200

    the reset point

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AddPost from "../ui/AddPost";
+import UserUpdate from "../ui/UserUpdate";
+
 export default function UserPage() {
   return (
     <>
       <h1>UserPage</h1>
       <hr />
       <AddPost />
+      <UserUpdate />
     </>
   );
 }
